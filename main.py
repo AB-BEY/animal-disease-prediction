94ff0a8a33b151f1333abdd75b05b330e7791723
Abbey <abiodunoyadina@gmail.com>
2025-02-28 16:24:45 +0100

    CORS fixed

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to your frontend URL
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
