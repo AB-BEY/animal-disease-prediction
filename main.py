84d741694712973a7d67860655689f1a9247a27c
Abbey <abiodunoyadina@gmail.com>
2025-03-02 09:57:08 +0100

    front end deployed url added

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 from database import create_tables
 from routes import auth, detect, add_pet
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",
+    "https://vet-vista-am5q.onrender.com"
 ]
 
 app.add_middleware(
@@ -17,14 +16,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.options("/{full_path:path}")
-async def preflight_handler(full_path: str):
-    response = JSONResponse(content={"message": "Preflight request allowed"})
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "OPTIONS, GET, POST, PUT, DELETE"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(detect.router, prefix="/detect", tags=["Detection"])
