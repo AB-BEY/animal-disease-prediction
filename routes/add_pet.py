1086089ea372a0ab59753942aa6bebbb4904779b
Abbey <abiodunoyadina@gmail.com>
2025-03-05 18:16:31 +0100

    fixed 2nd timee

--- routes/add_pet.py
+++ routes/add_pet.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from database import get_db
-from models import Animal
+from models import Animal, User
 from schemas import AnimalCreate, AllPetResponse, AnimalResponse
+from auth import get_current_user
 
 router = APIRouter()
 
@@ -19,6 +20,6 @@ async def create_animal(animal: AnimalCreate, db: Session = Depends(get_db)):
     return new_animal
 
 @router.get("/addpet", response_model=list[AllPetResponse])
-async def get_all_pets(current_user_id: int = Depends(get_current_user_id),db: Session = Depends(get_db)):
-    all_pets = db.exec(select(Animal).where(Animal.user_id == current_user_id)).all()
+async def get_all_pets(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    all_pets = db.exec(select(Animal).where(Animal.user_id == current_user.id)).all()
     return all_pets
