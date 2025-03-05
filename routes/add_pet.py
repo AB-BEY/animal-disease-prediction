7300edb68674f423bba76edf3a499e9fdfe57899
Abbey <abiodunoyadina@gmail.com>
2025-03-05 16:38:25 +0100

    fixed 2nd time

--- routes/add_pet.py
+++ routes/add_pet.py
@@ -18,7 +18,7 @@ async def create_animal(animal: AnimalCreate, db: Session = Depends(get_db)):
     db.refresh(new_animal)
     return new_animal
 
-@router.get("/addpet/{user_id}", response_model=list[AllPetResponse])
-async def get_all_pets(user_id: int,db: Session = Depends(get_db)):
-    all_pets = db.exec(select(Animal).where(Animal.user_id == user_id)).all()
+@router.get("/addpet", response_model=list[AllPetResponse])
+async def get_all_pets(current_user_id: int = Depends(get_current_user_id),db: Session = Depends(get_db)):
+    all_pets = db.exec(select(Animal).where(Animal.user_id == current_user_id)).all()
     return all_pets
--- routes/auth.py
+++ routes/auth.py
@@ -52,7 +52,7 @@ async def login(user: UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     # Generate JWT token
-    access_token = create_access_token(data={"sub": db_user.email}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(data={"sub": str(db_user.id)}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
 
     response_data = TokenResponse(
         access_token=access_token,
@@ -70,18 +70,19 @@ async def login(user: UserLogin, db: Session = Depends(get_db)):
 
     return response
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"},)
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
-        if email is None:
+        user_id = payload.get("sub")
+        if not user_id:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
 
-    user = db.exec(select(User).where(User.email == email)).first()
+    user = db.exec(select(User).where(User.id == user_id)).first()
     if not user:
         raise credentials_exception
 
